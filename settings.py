e53edd8991d79eb8e3e51c0c3e4d66ffa2772384
ivan <i.o.potapenko@gmail.com>
2018-11-14 01:57:13 +0100

    JSON comments enabled

--- settings.py
+++ settings.py
@@ -7,5 +7,7 @@ def load(filename):
     global settings
 
     print("Loading config file \"{}\"".format(filename))
+
     with open(filename, "r") as f:
-         settings = json.load(f)
+        # NB! this will fail if // is contained within a comment
+        settings = json.loads('\n'.join([row for row in f.readlines() if len(row.split('//')) == 1]))
